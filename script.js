b4124ea171fc7fdb786c2c8952d22e37431bf455
Fathyy <fathiabdi254@gmail.com>
2023-10-26 09:31:53 +0300

    Created contact page

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ faBars.onclick = function(){
 faXmark.onclick = function(){
     sideMenu.style.right = "-200px";   
 }
+
+// Google sheets form submission
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwAgJlLpSHgUVIjyiG8AaMwntJqQw0xTQYNWAQND2Emxe7r19jxFGOQ-f_eVARdgqyIUA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+        msg.innerHTML = "Thank you, your response has been submitted";
+        setTimeout(() => {
+          msg.innerHTML = "";
+        }, 5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
